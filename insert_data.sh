1feed97274431744573b8a11ea3cf602bfcc5932
Eddison Pinedo <91136098+EddisonPinedoEsp@users.noreply.github.com>
2025-02-12 12:17:51 -0500

    Add files via upload

---
+++ insert_data.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo $($PSQL "TRUNCATE games, teams")
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+  if [[ $WINNER != "winner" && $OPPONENT != "opponent" ]]
+  then
+    # get winner_id and opponent_id
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
+
+    if [[ -z $WINNER_ID ]]
+    then
+      # insert team
+      INSERT_WINNER_R=$($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
+      
+      if [[ $INSERT_WINNER_R == "INSERT 0 1" ]]
+      then 
+        echo "Inserted into teams, $WINNER"
+      fi
+
+      # get new winner_id
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    fi
+
+    # not found opponent_id
+    if [[ -z $OPPONENT_ID ]]
+    then
+      # insert team
+      INSERT_OPPONENT_R=$($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
+      
+      if [[ $INSERT_OPPONENT_R == "INSERT 0 1" ]]
+      then 
+        echo "Inserted into teams, $OPPONENT"
+      fi
+
+      # get new opponent_id
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    fi
+
+    # insert in games
+    INSERT_RE=$($PSQL "INSERT INTO games(year, round, winner_goals, opponent_goals, winner_id, opponent_id) VALUES ($YEAR, '$ROUND', $WINNER_GOALS, $OPPONENT_GOALS, $WINNER_ID, $OPPONENT_ID)")
+
+    if [[ $INSERT_RE == "INSERT 0 1" ]]
+    then 
+      echo "Inserted into games, $YEAR $ROUND $WINNER $OPPONENT $WINNER_GOALS $OPPONENT_GOALS"
+    fi
+  fi
+done
